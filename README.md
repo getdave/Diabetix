d1ceab3c0a57b7a5342a81bde7e618d60d8cb609
David <getdavemail@gmail.com>
2013-09-07 09:54:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Diabetix
+========
+
+Web app to facilitate simple carbohydrate counting and insulin to carb ratio calculations.
---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global diabetix, $*/
+
+
+window.diabetix = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    diabetix.init();
+});
