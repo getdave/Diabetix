6763a42e3a6eb32610b6ca87e298b2ed993c343d
David <getdavemail@gmail.com>
2013-09-14 09:46:13 +0100

    Added ability to set the query to the Foods API. Stil harded coded value. Updated view to display new data.

--- app/scripts/collections/foods.js
+++ app/scripts/collections/foods.js
@@ -7,9 +7,23 @@ diabetix.Collections = diabetix.Collections || {};
 
     diabetix.Collections.Foods = Backbone.Collection.extend({
 
+    	initialize: function(options) {
+    		this.searchQuery = "Bread";
+    	},
+        
         model: diabetix.Models.Food,
-
-        url: "https://api.nutritionix.com/v1_1/search/taco?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=207894da&appKey=9ceaebe161080c356ff15eaf2be38533",
+        
+        defaultParams: {
+        	"appId": "207894da", 
+        	"appKey": "9ceaebe161080c356ff15eaf2be38533",
+        	"fields" : "*"//"item_id,item_name,item_description,nf_total_carbohydrate"
+        },
+        
+    	params: {},
+    	url: function() {
+			return "https://api.nutritionix.com/v1_1/search/" + this.searchQuery + "/?" + $.param(_.defaults(this.params, this.defaultParams));
+		},
+        /* url: "https://api.nutritionix.com/v1_1/search/taco?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id", */
 
         parse: function (response) {
 		    return response.hits;
