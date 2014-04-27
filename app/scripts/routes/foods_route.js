1da4e65c90a6f0784085689ce1a544b13930f713
David <getdavemail@gmail.com>
2014-04-27 22:42:37 +0100

    Added layoutView for fixed header-secondary. Insert searchbox within header secondary.

--- app/scripts/routes/foods_route.js
+++ app/scripts/routes/foods_route.js
@@ -1,5 +1,12 @@
 Diabetix.FoodsRoute = Ember.Route.extend({
 	afterModel: function(model, transition) {
 		Diabetix.TitleView.set('title', "Foods");
-	}
+	},
+	renderTemplate: function(){
+        this.render();
+        this.render("_searchbox", {
+        	outlet: "header-secondary",
+        	into: 'application'
+        });
+    },
 });
---
+++ app/scripts/views/header_secondary_view.js
@@ -0,0 +1,4 @@
+Diabetix.HeaderSecondaryView = Ember.View.extend({
+  layoutName: 'layouts/header-secondary',
+  classNames: ["bar", "bar-standard", "bar-header-secondary"]
+});
